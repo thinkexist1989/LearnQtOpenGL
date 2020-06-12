61c9579df3aee2ea2b77a967a0c9effcf55bb3a2
Yang.LUO <992534544@qq.com>
2020-06-12 09:18:11 +0800

    delete build dir

--- cmake-build-debug/CMakeFiles/1.getting_started_2.2.hello_triangle_indexed_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "1.getting_started_2.2.hello_triangle_indexed_autogen/mocs_compilation.cpp"
-  "CMakeFiles/1.getting_started_2.2.hello_triangle_indexed_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/1.getting_started_2.2.hello_triangle_indexed_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
